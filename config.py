6d096d368da27071875e633e2356e1114f093c47
Nitya Oberoi <nitya@yipit.com>
2013-11-18 15:08:17 +0800

    Trying sendgrid heroku sending

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
-MAIL_SERVER = "smtp.gmail.com"
-MAIL_PORT = 465
-MAIL_USE_SSL = True
-MAIL_USERNAME = ""
-MAIL_PASSWORD = ""
+MAIL_SERVER = "smtp.sendgrid.net"
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+MAIL_USERNAME = "app19380888@heroku.com"
+MAIL_PASSWORD = "gfvlctuw"
 
-RECIPIENTS = ["info@aman-investment.com", "walidelasmar@aman-investment.com", "muhanadaliqabi@aman-investment.com"]
+# RECIPIENTS = ["info@aman-investment.com", "walidelasmar@aman-investment.com", "muhanadaliqabi@aman-investment.com"]
+RECIPIENTS = ["nitya.oberoi@gmail.com"]
+
 
 SECRET_KEY = '\x92\x1b"\x88F4\x10\xa3t)vp\xef\xac\xad\xed\x089\x05`\xfb.\x86\xa6\x98'
 DEBUG = False
